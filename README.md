4e7490f3f61b4384bb10814d7911a89b12153127
Morel Sébastien <morel.seb@gmail.com>
2020-03-17 11:49:35 -0700

    Update for Symfony 5

--- README.md
+++ README.md
@@ -6,24 +6,7 @@ bundle.
 It is useful when to want to automate an application deployment from a bundle
 repository.
 
-It has been develop to boostrap an eZ Platform 2.x application on Platform.sh
-
-> Note: Using Symfony 4 and Flex, this could be even more simplified.
-(wanna contribute? your are welcome)
-
-Ex:
-
-```
-- YourAwesomeBundle
-     - .platform
-     - bundle
-     - lib
-     - tests
-         - platform.sh
-            - bundles.yaml
-            - configs.yaml
-            - routes.yaml
-```
+It has been develop to boostrap an eZ Platform 2+ application locally and on Platform.sh 
 
 So the script will require
 
@@ -31,18 +14,52 @@ So the script will require
 - `configs.yaml` to automatically add the configuration in the application
 - `routes.yaml` to automatically add the routes in the application
 
+
+The folder structure would be:
+
+```
+- YourAwesomeBundle
+     - .platform
+     - bundle
+     - lib
+     - tests
+         - provisioning
+            - bundles.yaml
+            - configs.yaml
+            - routes.yaml
+```
+
+## Using Makefile (locally)
+
+```
+.PHONY: installez
+installez: ## Install eZ as the local project
+	@docker run -d -p 3366:3306 --name ezdbbundlecontainer -e MYSQL_ROOT_PASSWORD=ezplatform mariadb:10.3
+	@composer create-project ezsystems/ezplatform --prefer-dist --no-progress --no-interaction --no-scripts ezplatform
+	@curl -o tests/provisioning/wrap.php https://raw.githubusercontent.com/Plopix/symfony-bundle-app-wrapper/master/wrap-bundle.php
+	@WRAP_APP_DIR=./ezplatform WRAP_BUNDLE_DIR=./ php tests/provisioning/wrap.php
+	@rm tests/provisioning/wrap.php
+	@echo "Please set up this way:"
+	@echo "\tenv(DATABASE_HOST)     -> 127.0.0.1"
+	@echo "\tenv(DATABASE_PORT)     -> 3366"
+	@echo "\tenv(DATABASE_PASSWORD) -> ezplatform"
+	@cd $(EZ_DIR) && COMPOSER_MEMORY_LIMIT=-1 composer update --lock
+	@cd $(EZ_DIR) && bin/console ezplatform:install clean
+	@cd $(EZ_DIR) && bin/console cache:clear
+```
+
+
+## Using Platform.sh
+
 Then the deploy will looks like
 
 ```bash
 git clone YourAwesomeBundle.git
 composer create-project ezsystems/ezplatform --prefer-dist --no-progress --no-interaction --no-scripts
-curl -o tests/platform.sh/wrap.php https://raw.githubusercontent.com/Plopix/symfony-bundle-app-wrapper/master/wrap-bundle.php
-WRAP_ROOT_DIR=./ezplatform php tests/platform.sh/wrap.php
+curl -o tests/provisioning/wrap.php https://raw.githubusercontent.com/Plopix/symfony-bundle-app-wrapper/master/wrap-bundle.php
+WRAP_ROOT_DIR=./ezplatform php tests/provisioning/wrap.php
 cd ezplatform
 composer update --lock
 ```
-
-Open Source Examples:
--
 
 [License](LICENSE)
--- wrap-bundle.php
+++ wrap-bundle.php
@@ -5,59 +5,108 @@
  * bundle.
  *
  * @author    Sébastien Morel (Plopix) <morel.seb@gmail.com>
- * @copyright 2018 Sébastien Morel (Plopix)
+ * @copyright 2020 Sébastien Morel (Plopix)
  * @license   https://github.com/Plopix/symfony-bundle-app-wrapper/blob/master/LICENSE
  */
 
 $appRootDir = $_SERVER['WRAP_APP_DIR'] ?? null;
-$bundleDir  = $_SERVER['WRAP_BUNDLE_DIR'] ?? getenv('PLATFORM_DIR') ?? '~';
+$bundleDir = $_SERVER['WRAP_BUNDLE_DIR'] ?? getenv('PLATFORM_DIR') ?? '~';
 
 if (null === $appRootDir) {
     echo "WRAP_APP_DIR must be defined.";
 }
 
-// Inject the needed bundles in the Kernel
-// With 4 and Flex we should be able to do better
-$kernelFilePath = "{$appRootDir}/app/AppKernel.php";
-$kernel         = file_get_contents("{$appRootDir}/app/AppKernel.php");
-$bundles        = array_map(
-    function ($line) {
-        list($useless, $fqdn) = explode(" ", $line);
-        return '$bundles[] = new '.trim($fqdn).';';
-    },
-    file(__DIR__."/bundles.yaml")
-);
-$bundles[]      = 'return $bundles;';
-$kernel         = \str_replace($bundles[count($bundles) - 1], implode(PHP_EOL, $bundles), $kernel);
-file_put_contents($kernelFilePath, $kernel);
+if (file_exists("{$appRootDir}/app/AppKernel.php")) {
+    // this is Symfony 4
+    handleSymfony4($appRootDir);
+}
 
-// Inject the configurations
-// With 4 and Flex we should be able to do better
-file_put_contents("{$appRootDir}/app/config/config.yml", file_get_contents(__DIR__."/configs.yaml"), FILE_APPEND);
+if (file_exists("{$appRootDir}/src/Kernel.php")) {
+    // this is Symfony 5
+    handleSymfony5($appRootDir);
+}
 
-// Inject the routes
-// With 4 and Flex we should be able to do better
-file_put_contents("{$appRootDir}/app/config/routing.yml", file_get_contents(__DIR__."/routes.yaml"), FILE_APPEND);
+trickComposer($appRootDir, $bundleDir);
 
-// Trick the Composer.json
-$composerJsonPath   = "{$appRootDir}/composer.json";
-$data               = json_decode(file_get_contents($composerJsonPath), true);
-$bundleComposerJson = json_decode(file_get_contents("{$bundleDir}/composer.json"), true);
+function handleSymfony5(string $appRootDir)
+{
+    $bundleFile = "{$appRootDir}/config/bundles.php";
+    $bundleList = file_get_contents($bundleFile);
+    $bundles = array_map(
+        function ($line) {
+            $fqdn = trim(trim($line, "- "));
 
-// re map paths, one directory before because composer create-project install in a dir
-$psr4 = array_map(
-    function ($item) {
-        return "../{$item}";
-    },
-    $bundleComposerJson["autoload"]['psr-4'] ?? []
-);
+            return "{$fqdn}::class => [ 'all'=> true ],";
+        },
+        file(__DIR__."/bundles.yaml")
+    );
 
-$data["autoload"]['psr-4'] += $psr4;
-$data['require']           += $bundleComposerJson['require'] ?? [];
-$data['require-dev']       += $bundleComposerJson['require-dev'] ?? [];
+    $bundleList = \str_replace('];', implode(PHP_EOL, $bundles).PHP_EOL.'];', $bundleList);
+    file_put_contents($bundleFile, $bundleList);
 
-// write the composer.json
-file_put_contents(
-    $composerJsonPath,
-    json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)
-);
+    $bundleName = substr(trim(file(__DIR__."/configs.yaml")[0]), 0, -1);
+
+    // Inject the configurations
+    file_put_contents(
+        "{$appRootDir}/config/packages/{$bundleName}.yaml",
+        file_get_contents(__DIR__."/configs.yaml")
+    );
+
+    // Inject the routes
+    file_put_contents(
+        "{$appRootDir}/config/routes/{$bundleName}.yaml",
+        file_get_contents(__DIR__."/routes.yaml")
+    );
+}
+
+function trickComposer(string $appRootDir, string $bundleDir)
+{
+    $composerJsonPath = "{$appRootDir}/composer.json";
+    $data = json_decode(file_get_contents($composerJsonPath), true);
+    $bundleComposerJson = json_decode(file_get_contents("{$bundleDir}/composer.json"), true);
+
+    // re map paths, one directory before because composer create-project install in a dir
+    $psr4 = array_map(
+        function ($item) {
+            return "../{$item}";
+        },
+        $bundleComposerJson["autoload"]['psr-4'] ?? []
+    );
+
+    $data["autoload"]['psr-4'] += $psr4;
+    $data['require'] += $bundleComposerJson['require'] ?? [];
+    $data['require-dev'] += $bundleComposerJson['require-dev'] ?? [];
+
+    // write the composer.json
+    file_put_contents(
+        $composerJsonPath,
+        json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)
+    );
+}
+
+function handleSymfony4(string $appRootDir)
+{
+    $kernelFilePath = "{$appRootDir}/app/AppKernel.php";
+    $kernel = file_get_contents($kernelFilePath);
+    $bundles = array_map(
+        function ($line) {
+            $fqdn = trim(trim($line, "- "));
+
+            return '$bundles[] = new '.$fqdn.';';
+        },
+        file(__DIR__."/bundles.yaml")
+    );
+    $bundles[] = 'return $bundles;';
+    $kernel = \str_replace($bundles[count($bundles) - 1], implode(PHP_EOL, $bundles), $kernel);
+    file_put_contents($kernelFilePath, $kernel);
+
+    // Inject the configurations
+    file_put_contents("{$appRootDir}/app/config/config.yml", file_get_contents(__DIR__."/configs.yaml"), FILE_APPEND);
+
+    // Inject the routes
+    file_put_contents("{$appRootDir}/app/config/routing.yml", file_get_contents(__DIR__."/routes.yaml"), FILE_APPEND);
+}
+
+
+
+
